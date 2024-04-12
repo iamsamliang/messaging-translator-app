01bbd2fffb583c384ad1b5f2abaa322a43253713
Sam Liang <darkdragn20000@gmail.com>
2024-04-11 20:11:29 -0400

    login on client side

--- frontend/src/routes/(home)/login/+page.svelte
+++ frontend/src/routes/(home)/login/+page.svelte
@@ -1,17 +1,57 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
+	import clientSettings from '$lib/config/config.client';
 
-	export let form;
+	let loginForm: HTMLFormElement;
+	let incorrect: boolean = false;
+	let unverified: boolean = false;
+	let errorMsg: string = '';
+
+	async function handleLogin() {
+		const formData = new FormData(loginForm);
+
+		try {
+			const response: Response = await fetch(`${clientSettings.apiBaseURL}/login/access-token`, {
+				method: 'POST',
+				body: formData,
+				credentials: 'include'
+			});
+
+			if (!response.ok) {
+				const errorResponse = await response.json();
+
+				if (response.status === 401) {
+					incorrect = true;
+					return;
+				} else if (response.status === 412) {
+					unverified = true;
+					return;
+				}
+				throw new Error(errorResponse.detail);
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				errorMsg = error.message;
+			}
+
+			// Handle other cases, defaulting to a generic message
+			errorMsg = 'An unknown error occurred';
+			return;
+		}
+
+		goto('/chat');
+	}
 </script>
 
 <div class="bg-black text-white flex flex-col justify-center items-center mt-5">
 	<h1 class="text-3xl font-bold mb-3">Log In</h1>
-	{#if form?.incorrect}
+	{#if incorrect}
 		<div class="border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
 			<strong class="font-bold">Error:</strong>
 			<span class="block sm:inline">Invalid email or password</span>
 		</div>
-	{:else if form?.unverified}
+	{:else if unverified}
 		<div class="border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
 			<strong class="font-bold">Error:</strong>
 			<span class="block sm:inline"
@@ -22,17 +62,17 @@
 				></span
 			>
 		</div>
-	{:else if form?.error}
+	{:else if errorMsg}
 		<div class="border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
 			<strong class="font-bold">Error:</strong>
-			<span class="block sm:inline">{form.error}</span>
+			<span class="block sm:inline">{errorMsg}</span>
 		</div>
 	{/if}
 	<form
 		class="bg-gray-900 w-[85%] min-[450px]:w-[25rem] shadow-md rounded-xl px-8 py-6 mb-5"
-		method="POST"
-		action="?/login"
+		on:submit|preventDefault={handleLogin}
 		use:enhance
+		bind:this={loginForm}
 	>
 		<div class="mb-4">
 			<label for="email" class="block text-sm font-bold mb-2"> Email </label>
